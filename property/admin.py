b8ee30b9792280b1758323ac93903b1716c1c8b5
den1slx <keycoingold@mail.ru>
2023-04-28 19:35:34 +1000

    Setup filtering in admin UI

--- property/admin.py
+++ property/admin.py
@@ -8,6 +8,7 @@ class AdminFlat(admin.ModelAdmin):
 
     list_display = ('address', 'price', 'new_building', 'construction_year', 'town')
     list_editable = ('new_building',)
-    search_fields = ['town', 'address']
+    list_filter = ('active', 'new_building', 'rooms_number', 'has_balcony', 'floor',)
+    search_fields = ['town', 'address', 'construction_year']
     readonly_fields = ['created_at']
 
